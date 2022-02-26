961b4c70226fd30bcbd88d33c969a7291b81507d
Reinaldo Junior <rsljunior@live.com>
2022-02-26 18:47:02 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.CurrencyConverter;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("What is the dollar prices?: ");
+		double valorDollar = sc.nextDouble();
+
+		System.out.println("How many dollars will be bought?: ");
+		double valorReais = sc.nextDouble();
+
+		double valorConversao = CurrencyConverter.conversaoDollar(valorDollar, valorReais);
+
+		System.out.printf("Amount to be paid in reais R$: %.2f%n", valorConversao);
+
+		sc.close();
+
+	}
+
+}
